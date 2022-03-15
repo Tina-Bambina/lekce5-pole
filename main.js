373dcf2a8eff82c8501b77819f710da6bd4b6a53
Tina-Bambina <speleerska@gmail.com>
2022-03-15 20:18:31 +0100

    hotove cviceni z hodiny

--- main.js
+++ main.js
@@ -6,22 +6,30 @@
 // Úkol 1 - Založ nové pole se seznamem jmen (alespoň 5)
 // Vypiš jeho obsah do stránky - JavaScript spojí pole automaticky do řetězce
 
-
-
-
+let pole = [
+    "Markéta",
+    "Kateřina",
+    "Júlia",
+    "Lucie",
+    "Veronika"
+]
+pole;
+console.log(pole);
+vypisText(pole);
 
 // Úkol 2 - Vypiš třetí jméno v pořadí
 // Nezapomeň, že pole se čísluje od 0
 
-
-
+pole[2];
+vypisText(pole[2]);
 
 
 
 // Úkol 3 - Kolik jmen je v poli?
 // Pole má vlastnost pole.length
 
-
+pole.length;
+vypisText(pole.length);
 
 
 
@@ -35,6 +43,12 @@
 
 
 
+function vypisJmenaSPoradim(ele, idx){
+vypisText(idx + '. ' + ele)
+}
+pole.forEach(vypisJmenaSPoradim);
+
+
 
 
 // Úkol 5 - přidej na konec pole další jméno
@@ -43,7 +57,8 @@
 // Doplněný seznam jmen vypiš (jednoduše jako v příkladu 1)
 
 
-
+pole.push('Posledni Pani');
+vypisText(pole[5]);
 
 
 // Úkol 6 - odeber z pole první a poslední jméno a vypiš
@@ -51,16 +66,22 @@
 // pole.shift()
 // pole.pop()
 
+pole[0];
+pole[5];
+vypisText(pole[0]);
+vypisText(pole[5]);
 
-
-
-
-
-
-
-
+pole.shift()
+pole.pop()
+vypisText(pole);
 
 // *********************************************
 function vypisText(txt) {
     document.querySelector('#vystup').innerText += txt + '\n';
+    pole[0];
 }
+
+
+// function vypisTextSIndexem(element, index) {
+//     document.querySelector('#vystup').innerText += index + '. ' + element + '\n
+// }
